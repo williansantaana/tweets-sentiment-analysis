85353992b1ee83bc85d5c7f555dd2f5130d1c82b
Willian Santa Ana <willian.ana@pdmfc.com>
2025-03-20 23:05:13 +0000

    captioning with blip

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from utils.database import execute_query
-from utils.image_process import preprocess_image, extract_text_from_image, generate_caption_blip
+from utils.image_process import generate_caption_blip
+import sys
 
 
 def main():
-    select_query = "SELECT id, pub_id, pub_img FROM tweets WHERE pub_img IS NOT NULL AND pub_img_text IS NULL AND pub_img_caption IS NULL LIMIT 100"
-    update_query = "UPDATE tweets SET pub_img_text = %s, pub_img_caption = %s WHERE id = %s"
+    if len(sys.argv) <= 1:
+        print("Nenhum parâmetro foi passado.")
+        sys.exit()
+
+    order_by = sys.argv[1]
+
+    if order_by not in ["ASC", "DESC"]:
+        print("Parâmetros aceitos: ASC, DESC.")
+        sys.exit()
+
+    select_query = f"SELECT id, pub_id, pub_img FROM tweets WHERE pub_img IS NOT NULL AND pub_img_caption IS NULL ORDER BY {order_by} LIMIT 100"
+    update_query = "UPDATE tweets SET pub_img_caption = %s WHERE id = %s"
 
     while True:
         tweets = execute_query(select_query)
 
-        if len(tweets) == 0:
-            break
+        if len(tweets) == 0: break
 
         for tweet in tweets:
-            processed_image = preprocess_image(tweet['pub_img'])
-            image_text = extract_text_from_image(preprocess_image)
-            image_caption = generate_caption_blip(tweet['pub_img'])
-
-            execute_query(update_query, (image_text, image_caption, tweet['id']))
+            try:
+                image_caption = generate_caption_blip(tweet['pub_img'])
+                execute_query(update_query, (image_caption, tweet['id']))
+            except Exception as e:
+                execute_query(update_query, ("No image", tweet['id']))
 
 
 
